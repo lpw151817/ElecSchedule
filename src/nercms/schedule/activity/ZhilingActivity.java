46ada735dce9facde2b0ca7b5420c12ed7d249a2
Totoy <Totoy@Totoy-PC>
2016-03-17 17:16:16 +0800

    指令list

--- src/nercms/schedule/activity/ZhilingActivity.java
+++ src/nercms/schedule/activity/ZhilingActivity.java
@@ -2,37 +2,28 @@ package nercms.schedule.activity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.Inflater;
 
-import android.app.Activity;
-import android.content.Intent;
+import nercms.schedule.R;
+import nercms.schedule.adapter.ZhilingAdapter;
+import nercms.schedule.view.NoScrollViewPager;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.wxapp.service.elec.dao.TaskInsDao;
 import android.wxapp.service.elec.model.bean.table.tb_task_instructions;
-import nercms.schedule.R;
-import nercms.schedule.R.id;
-import nercms.schedule.R.layout;
-import nercms.schedule.R.menu;
-import nercms.schedule.adapter.ZhilingAdapter;
-import nercms.schedule.view.NoScrollViewPager;
+import android.wxapp.service.elec.model.bean.table.tb_task_instructions_receive;
 
 public class ZhilingActivity extends BaseActivity implements OnClickListener {
 
 	ListView listview;
 	ZhilingAdapter adapter;
-	TaskInsDao dao;
-	List<tb_task_instructions> data;
+	List<tb_task_instructions> unReadData = new ArrayList<tb_task_instructions>();
+	List<tb_task_instructions> readData = new ArrayList<tb_task_instructions>();
 
 	List<View> list = new ArrayList<View>();
 	private View tab01;
@@ -43,6 +34,11 @@ public class ZhilingActivity extends BaseActivity implements OnClickListener {
 	private PagerAdapter adapter2;
 	private ListView mUnReadListView;
 	private ListView mReadListView;
+	private List<tb_task_instructions_receive> listUnread;
+	private List<tb_task_instructions_receive> listRead;
+	private TaskInsDao dao;
+	private ZhilingAdapter unReadAdapter;
+	private ZhilingAdapter readAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,36 +48,36 @@ public class ZhilingActivity extends BaseActivity implements OnClickListener {
 		iniActionBar(true, null, "¡Ÿ ±÷∏¡Ó");
 
 		initViews();
-		
+
 		viewPager = (NoScrollViewPager) findViewById(R.id.lv_viewpager);
-		
+
 		adapter2 = new PagerAdapter() {
-			
+
 			@Override
 			public boolean isViewFromObject(View arg0, Object arg1) {
 				return arg0 == arg1;
 			}
-			
+
 			@Override
 			public int getCount() {
 				return list.size();
 			}
-			
+
 			@Override
 			public void destroyItem(ViewGroup container, int position,
-					Object object) {//…æ≥˝“≥ø®
+					Object object) {// …æ≥˝“≥ø®
 				container.removeView(list.get(position));
 			}
 
 			@Override
 			public Object instantiateItem(ViewGroup container, int position) {
-				View view = list.get(position);//ÃÌº”“≥ø®
+				View view = list.get(position);// ÃÌº”“≥ø®
 				container.addView(view);
 
 				return view;
 			}
 		};
-		
+
 		viewPager.setAdapter(adapter2);
 
 		// dao = new TaskInsDao(this);
@@ -116,10 +112,31 @@ public class ZhilingActivity extends BaseActivity implements OnClickListener {
 
 		mUnRead.setOnClickListener(this);
 		mRead.setOnClickListener(this);
-		
-		mUnReadListView = (ListView) tab01.findViewById(R.id.lv_unread);
-		mReadListView = (ListView) tab01.findViewById(R.id.lv_read);
 
+		mUnReadListView = (ListView) tab01.findViewById(R.id.lv_unread);
+		mReadListView = (ListView) tab02.findViewById(R.id.lv_read);
+
+		dao = new TaskInsDao(this);
+		listUnread = dao.getAllIns(getUserId(), "0");
+		listRead = dao.getAllIns(getUserId(), "1");
+
+//			unReadData.add(dao.getTaskIns(listUnread));
+			unReadData = dao.getTaskIns(listUnread);
+
+			readData = dao.getTaskIns(listRead);
+		// for (tb_task_instructions_receive instructions : listRead) {
+		// readData.add(dao.getTaskIns(instructions.getInstructions_id()));
+		// }
+		
+		unReadAdapter = new ZhilingAdapter(this, unReadData);
+		readAdapter = new ZhilingAdapter(this, readData);
+		
+		mUnReadListView.setAdapter(unReadAdapter);
+		mReadListView.setAdapter(readAdapter);
+		
+
+		// public tb_task_instructions getTaskIns(String taskInsId) {
+		// List<tb_task_instructions> data;
 	}
 
 	@Override
@@ -127,28 +144,28 @@ public class ZhilingActivity extends BaseActivity implements OnClickListener {
 		switch (v.getId()) {
 		case R.id.tv_unread:
 			viewPager.setCurrentItem(0);
-			mUnRead.setTextColor(getResources().getColor(R.color.specialgreen));
-			mRead.setTextColor(getResources().getColor(R.color.specialgray));
+			mUnRead.setTextColor(getResources().getColor(R.color.blue));
+			mRead.setTextColor(getResources().getColor(R.color.black));
 			break;
 
 		case R.id.tv_read:
 			viewPager.setCurrentItem(1);
-			mUnRead.setTextColor(getResources().getColor(R.color.specialgray));
-			mRead.setTextColor(getResources().getColor(R.color.specialgreen));
+			mUnRead.setTextColor(getResources().getColor(R.color.black));
+			mRead.setTextColor(getResources().getColor(R.color.blue));
 			break;
 		default:
 			break;
 		}
 	}
 
-	// @Override
-	// public boolean onOptionsItemSelected(com.actionbarsherlock.view.MenuItem
-	// item) {
-	// switch (item.getItemId()) {
-	// case android.R.id.home:
-	// finish();
-	// break;
-	// }
-	// return super.onOptionsItemSelected(item);
-	// }
+	@Override
+	public boolean onOptionsItemSelected(
+			com.actionbarsherlock.view.MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
