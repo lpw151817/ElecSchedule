ea158d0fb6c5266fbc4c6716fe03316dd4055860
Totoy <Totoy@Totoy-PC>
2016-03-17 14:41:55 +0800

    主页界面

--- src/nercms/schedule/activity/ZhilingActivity.java
+++ src/nercms/schedule/activity/ZhilingActivity.java
@@ -1,10 +1,13 @@
 package nercms.schedule.activity;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,6 +28,10 @@ public class ZhilingActivity extends BaseActivity {
 	ZhilingAdapter adapter;
 	TaskInsDao dao;
 	List<tb_task_instructions> data;
+	
+	List<View> list = new ArrayList<View>();
+	private View tab01;
+	private View tab02;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,32 +39,41 @@ public class ZhilingActivity extends BaseActivity {
 		setContentView(R.layout.activity_zhiling);
 
 		iniActionBar(true, null, "¡Ÿ ±÷∏¡Ó");
+		
+		initViews();
 
-		dao = new TaskInsDao(this);
-		data = dao.getAllIns();
-
-		listview = (ListView) findViewById(R.id.listview);
-		adapter = new ZhilingAdapter(this, data);
-		listview.setAdapter(adapter);
-
-		listview.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Intent intent = new Intent(ZhilingActivity.this, NewTask.class);
-				intent.putExtra("taskInsId", data.get(position).getId());
-				startActivity(intent);
-			}
-		});
+//		dao = new TaskInsDao(this);
+//		data = dao.getAllIns();
+//
+//		listview = (ListView) findViewById(R.id.listview);
+//		adapter = new ZhilingAdapter(this, data);
+//		listview.setAdapter(adapter);
+//
+//		listview.setOnItemClickListener(new OnItemClickListener() {
+//
+//			@Override
+//			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//				Intent intent = new Intent(ZhilingActivity.this, NewTask.class);
+//				intent.putExtra("taskInsId", data.get(position).getId());
+//				startActivity(intent);
+//			}
+//		});
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(com.actionbarsherlock.view.MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			finish();
-			break;
-		}
-		return super.onOptionsItemSelected(item);
+	private void initViews() {
+		LayoutInflater inflater = LayoutInflater.from(this);
+		
+		tab01 = inflater.inflate(R.layout.activity_zhiling_tab01, null);
+		tab02 = inflater.inflate(R.layout.activity_zhiling_tab02, null);
 	}
+
+//	@Override
+//	public boolean onOptionsItemSelected(com.actionbarsherlock.view.MenuItem item) {
+//		switch (item.getItemId()) {
+//		case android.R.id.home:
+//			finish();
+//			break;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
 }
